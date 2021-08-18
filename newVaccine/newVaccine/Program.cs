9f3942a58d739b1e2d266d6ba9eae8a3ce5a9f61
vikkie1234 <venuspieters6@gmail.com>
2021-08-18 10:02:01 +0200

    Created form
    created the vaccince form

---
+++ newVaccine/newVaccine/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace newVaccine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name, surname, gender, province;
+            string DOB;
+
+            Console.WriteLine("Vaccince Registration Form");
+            Console.WriteLine(".................................");
+
+            Console.Write("Name: ");
+            name = Console.ReadLine();
+            Console.Write("Surname: ");
+            surname = Console.ReadLine();
+            Console.Write("Gender: ");
+            gender = Console.ReadLine();
+            Console.Write("Date of birth: ");
+            DOB = Console.ReadLine();
+
+            Console.Write("Province: ");
+
+            province = Console.ReadLine();
+        }
+    }
+}
